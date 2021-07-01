3bb94acf45e1d3c1e7f6e11a41513c05f98a9c80
Gilvane Freire <68249702+GilvaneFreire@users.noreply.github.com>
2021-07-01 11:57:44 -0300

    API C#

---
+++ TipoVeiculo.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace TesteApi
+{
+    public class TipoVeiculo
+    {
+        public string Codigo { get; set; }
+        public string Descricao { get; set; }
+    }
+}
---
+++ Truck.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TesteApi
+{
+    public class Truck
+    {
+        public int IdVeiculo { get; set; }
+        public string Cooperado { get; set; }
+        public string Motorista { get; set; }
+        public string Placa { get; set; }
+        public string Status { get; set; }
+        public string DataPacote { get; set; }
+        public string Viagem { get; set; }
+        public string Cliente { get; set; }
+        public string Destino { get; set; }
+        public string PrevEntrega { get; set; }
+        public string TipoVeiculo { get; set; }
+        public string NomeTipoVeiculo { get; set; }
+        public string PlacaCavalo { get; set; }
+        public double Cltlat { get; set; }
+        public double Cltlng { get; set; }
+        public string CidadeAtual { get; set; }
+        public string UFatual { get; set; }
+        public string Destinatario { get; set; }
+        public string DiasSemAtualizar { get; set; }
+
+    }
+}
